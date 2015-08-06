998e3d009707061a57055202e2eaf8408cfba933
KruchDmitriy <kruch.dmitriy@gmail.com>
2015-08-06 13:48:43 +0300

    work on learning curve

--- scripts/learning_curve.py
+++ scripts/learning_curve.py
@@ -1,70 +1,53 @@
 import numpy as np
 import re
 import argparse
-from matplotlib import pylab as plt
+from os import system, remove
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--files", required = True, help = "pathes to logs")
+    parser.add_argument("-f", "--file", required = True, help = "path to log")
 
     args = vars(parser.parse_args())
-    files = args["files"]
+    file = args["file"]
 
-    plt.style.use('ggplot')
-    fig, ax1 = plt.subplots()
-    ax2 = ax1.twinx()
-    ax1.set_xlabel('iteration')
-    ax1.set_ylabel('loss')
-    ax2.set_ylabel('accuracy %')
-    for i, log_file in enumerate(files):
-        loss_iterations, losses, accuracy_iterations, accuracies, accuracies_iteration_checkpoints_ind = parse_log(log_file)
-        disp_results(fig, ax1, ax2, loss_iterations, losses, accuracy_iterations, accuracies, accuracies_iteration_checkpoints_ind, color_ind=i)
-    plt.show()
+    pattern = r"Iteration (?P<iter_num>\d+), loss = (?P<loss_val>\d+\.\d+e?[+-]?\d+)"
+    result = parse_log(file, pattern, 2)
+    train = open("train.txt", "w")
+    for str_tmp in result:
+        train.write(str_tmp)
+
+    pattern = r"Iteration (?P<iter_num>\d+), Testing net \(#0\)\n.*\n.*\n.*\n.*\n.* Test net output #4: loss3/loss3 = (?P<loss3>\d+\.\d+e?[+-]?\d+).*\n.* Test net output #5: loss3/top-1 = (?P<accuracy>\d+\.\d+e?[+-]?\d+)"
+    result = parse_log(file, pattern, 3)
+    test = open("test.txt", "w")
+    for str_tmp in result:
+        test.write(str_tmp)
 
 
-def parse_log(log_file):
+def parse_log(log_file, pattern, num_objects):
     with open(log_file, 'r') as log_file:
         log = log_file.read()
 
-    loss_pattern = r"Iteration (?P<iter_num>\d+), loss = (?P<loss_val>[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?)"
-    losses = []
-    loss_iterations = []
+    objects = []
+    for r in re.findall(pattern, log):
+        str_tmp = ""
+        if num_objects == 1:
+            str_tmp = r
+        else:
+            for i in range(num_objects):
+                str_tmp += r[i] + " "
+            str_tmp += "\n"
+        objects.append(str_tmp)
+    return objects
 
-    for r in re.findall(loss_pattern, log):
-        loss_iterations.append(int(r[0]))
-        losses.append(float(r[1]))
-
-    loss_iterations = np.array(loss_iterations)
-    losses = np.array(losses)
-
-    accuracy_pattern = r"Iteration (?P<iter_num>\d+), Testing net \(#0\)\n.* accuracy = (?P<accuracy>[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?)"
-    accuracies = []
-    accuracy_iterations = []
-    accuracies_iteration_checkpoints_ind = []
-
-    for r in re.findall(accuracy_pattern, log):
-        iteration = int(r[0])
-        accuracy = float(r[1]) * 100
-
-        if iteration % 10000 == 0 and iteration > 0:
-            accuracies_iteration_checkpoints_ind.append(len(accuracy_iterations))
-
-        accuracy_iterations.append(iteration)
-        accuracies.append(accuracy)
-
-    accuracy_iterations = np.array(accuracy_iterations)
-    accuracies = np.array(accuracies)
-
-    return loss_iterations, losses, accuracy_iterations, accuracies, accuracies_iteration_checkpoints_ind
-
-
-def disp_results(fig, ax1, ax2, loss_iterations, losses, accuracy_iterations, accuracies, accuracies_iteration_checkpoints_ind, color_ind=0):
-    modula = len(plt.rcParams['axes.color_cycle'])
-    ax1.plot(loss_iterations, losses, color=plt.rcParams['axes.color_cycle'][(color_ind * 2 + 0) % modula])
-    ax2.plot(accuracy_iterations, accuracies, plt.rcParams['axes.color_cycle'][(color_ind * 2 + 1) % modula])
-    ax2.plot(accuracy_iterations[accuracies_iteration_checkpoints_ind], accuracies[accuracies_iteration_checkpoints_ind], 'o', color=plt.rcParams['axes.color_cycle'][(color_ind * 2 + 1) % modula])
-
+def plot(result_file, name):
+    tmp = open("tmp.gp", "w")
+    text = "set terminal png truecolor size 350, 300\n\
+            set output '" + name + ".png'\n\
+            set ylabel \"minutes\" font ", 20"\n\
+            set yrange [0:70]\n\
+            set title \"" + name + "\" font \", 20\"\
+            plot '" + result_file + "' using 1:2 with lines"
 
 if __name__ == '__main__':
     main()
