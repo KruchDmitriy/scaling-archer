fc0c82775dbb252000487ddad95bbe6c66d0c8c7
KruchDmitriy <kruch.dmitriy@gmail.com>
2015-08-06 16:54:54 +0300

    learning curves and fixed scripts

--- data_prep/create_label_caffe.py
+++ data_prep/create_label_caffe.py
@@ -1,24 +1,31 @@
+import argparse
 from random import shuffle
+  
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--path", required = True, help = "path to KITTI dataset")
+
+args = vars(parser.parse_args())
+path = args["path"]  
 
 train = []
 test = []
 
 for i in range(21798):
     if i < 4360:
-        test.append("/home/kruchinin_d/Projects/KITTI/prepared_data/training/%06d.png 1" % i)
+        test.append(path + ("/prepared_data/training/%06d.png 1" % i))
     else:
-        train.append("/home/kruchinin_d/Projects/KITTI/prepared_data/training/%06d.png 1" % i)
+        train.append(path + ("/prepared_data/training/%06d.png 1" % i))
 for i in range(89664):
     if i < 17439:
-        test.append("/home/kruchinin_d/Projects/KITTI/prepared_data/negatives/%06d.png 0" % i)
+        test.append(path + ("/prepared_data/negatives/%06d.png 0" % i))
     else:
-        train.append("/home/kruchinin_d/Projects/KITTI/prepared_data/negatives/%06d.png 0" % i)
+        train.append(path + ("/prepared_data/negatives/%06d.png 0" % i))
 
 shuffle(train)
 shuffle(test)
 
-test_file = open('test.txt', 'w')
-train_file = open('train.txt', 'w')
+test_file = open(path + '/prepared_data/test.txt', 'w')
+train_file = open(path + '/prepared_data/train.txt', 'w')
 
 for item in test:
     test_file.write("%s\n" % item)
--- scripts/learning_curve.py
+++ scripts/learning_curve.py
@@ -1,12 +1,14 @@
 import numpy as np
 import re
 import argparse
-from os import system, remove
+import subprocess
+from os import remove
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--file", required = True, help = "path to log")
+    parser.add_argument("-o", "--output", required = True, help = "path to plots")
 
     args = vars(parser.parse_args())
     file = args["file"]
@@ -16,12 +18,22 @@ def main():
     train = open("train.txt", "w")
     for str_tmp in result:
         train.write(str_tmp)
-
+    train.close()
+    
     pattern = r"Iteration (?P<iter_num>\d+), Testing net \(#0\)\n.*\n.*\n.*\n.*\n.* Test net output #4: loss3/loss3 = (?P<loss3>\d+\.\d+e?[+-]?\d+).*\n.* Test net output #5: loss3/top-1 = (?P<accuracy>\d+\.\d+e?[+-]?\d+)"
     result = parse_log(file, pattern, 3)
     test = open("test.txt", "w")
     for str_tmp in result:
         test.write(str_tmp)
+    test.close()
+
+    out = args["output"]
+    plot("train.txt", "1:2", out, "train", "loss", True)
+    plot("test.txt", "1:2", out, "test", "loss", True)
+    plot("test.txt", "1:3", out, "test_acc", "accuracy", False)
+
+    remove("train.txt")
+    remove("test.txt")
 
 
 def parse_log(log_file, pattern, num_objects):
@@ -40,14 +52,22 @@ def parse_log(log_file, pattern, num_objects):
         objects.append(str_tmp)
     return objects
 
-def plot(result_file, name):
+def plot(result_file, columns, path, name, ylabel, logscale):
     tmp = open("tmp.gp", "w")
-    text = "set terminal png truecolor size 350, 300\n\
-            set output '" + name + ".png'\n\
-            set ylabel \"minutes\" font ", 20"\n\
-            set yrange [0:70]\n\
-            set title \"" + name + "\" font \", 20\"\
-            plot '" + result_file + "' using 1:2 with lines"
+    sharp_ls = ""
+    if not logscale:
+	sharp_ls += "#"
+    text = "set terminal png truecolor size 1024, 512\n\
+            set output '" + path + "/" + name + ".png'\n\
+            " + sharp_ls + "set logscale y\n\
+            set xlabel 'iterations'\n\
+            set ylabel '" + ylabel + "' \n\
+            set title '" + name + "' \n\
+            plot '" + result_file + "' using " + columns + " with lines smooth bezier\n"
+    tmp.write(text)
+    tmp.close()
+    subprocess.call(["gnuplot", "tmp.gp"])
+    remove('tmp.gp')
 
 if __name__ == '__main__':
     main()
