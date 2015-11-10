bfb6b73c0b99ef0aa2279c19a8f28cc57d0ad24f
KruchDmitriy <kruch.dmitriy@gmail.com>
2015-11-10 23:57:24 +0300

    new models and results

---
+++ nn/bvlc_googlenet_parttune/run_train.sh
@@ -0,0 +1,1 @@
+srun -t "24:00:00" -o "/home/kruchinin_d/Projects/scaling-archer/nn/bvlc_googlenet_parttune/log.txt" -N 1 -p gpu /home/kruchinin_d/lib/caffe/build/tools/caffe train -solver /home/kruchinin_d/Projects/scaling-archer/nn/bvlc_googlenet_parttune/solver.prototxt -weights /home/kruchinin_d/Projects/scaling-archer/nn/bvlc_googlenet/bvlc_googlenet.caffemodel &
